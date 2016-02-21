aa858d140a07a7e75462f5715307c539efbee12f
steryoshkin <credo8@yandex.ru>
2016-02-21 17:58:16 +0700

    Update wms_proxy.php

--- fibers/wms_proxy.php
+++ fibers/wms_proxy.php
@@ -14,21 +14,12 @@
 	
 	if(empty($_SESSION['logged_user_fibers'])) {
 		echo 'reload';
-		//echo 'Обновите страницу';
 		die;
 	}
 	
 	if(isset($_GET['args'])) {
-		//$result = file_get_contents('http://'.$host.':8080/geoserver/opengeo/'.$_GET['args']);
 		$result = file_get_contents('http://localhost:8080/geoserver/opengeo/'.$_GET['args']);
-		//print_r($result);
 		$array = XML2Array::createArray($result);
-		/*echo '<pre>';
-		//print_r($result);
-		print_r($array);
-		echo '</pre>';
-		die;*/
-		
 		$content="";
 		$cable="";
 		$node="";
@@ -37,86 +28,55 @@
 			foreach ($array['wfs:FeatureCollection']['gml:featureMember'] as $value) {
 				$temp=@get_node($value['opengeo:node']);
 				if(!empty($temp)) {
-					//$node.='<table class="egg_table">'.$temp.'</table>';
 					$node.=$temp;
 				}
 
 				$temp=($group_access['map_type']==1?@get_cable($value['opengeo:cable']):'');
 				if(!empty($temp)) {
-					//$cable.='<table>'.$temp.'</table>';
 					$cable.=$temp;
 				}
 			}
 		} else {
 			$temp=get_node($array['wfs:FeatureCollection']['gml:featureMember']['opengeo:node']);
 			if(!empty($temp)) {
-				//$node='<table class="egg_table">'.$temp.'</table>';
 				$node.=$temp;
 			}
 
 			$temp=($group_access['map_type']==1?get_cable($array['wfs:FeatureCollection']['gml:featureMember']['opengeo:cable']):'');
 			if(!empty($temp))
-				//$cable='<table>'.$temp.'</table>';
 				$cable.=$temp;
 		}
-		//$close_button='<td><input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></td>';
-		//$close='<td><input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></td>';
 		$close='<input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');">';
 
 		if(!empty($node)) {
 			$node=preg_replace('/{CLOSE}/', $close, $node);
-			//echo $node;
 			$content.=$node;
 		}
 		if(!empty($cable)) {
-			//if(!empty($node))
-				//$content.='<hr>';
-				//echo '<hr>';
 			if(empty($node))
 				$cable=preg_replace('/{CLOSE}/', $close, $cable, 1);
 			$cable=preg_replace('/{CLOSE}/', '', $cable);
-			//echo $cable;
 			$content.=$cable;
 		}
 		if(!empty($content))
 			echo '<div class="egg_table_div">'.$content.'</div>';
-			//echo '<table class="egg_table">'.$content.'</table>';
 	}
 	
 	if(isset($_GET['geocode'])) {
-		//if($_SERVER['REMOTE_ADDR']=='192.168.6.12' || $_SERVER['REMOTE_ADDR']=='192.168.6.14') {
 		$addr=geocode_local($_GET['lat'],$_GET['lon'],$_GET['addressdetails']);
-		//echo $addr;
-
 			echo '<div style="float: left;">'.geocode_local($_GET['lat'],$_GET['lon'],$_GET['addressdetails']).'</div>
 					<div style="float: right;">&nbsp;<input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></input></div><br>';
-			//echo '<div><a href="http://maps.yandex.ru/?text='.$addr.'&l=map" target="_blank">Организации</a></div>';
-			//echo '<a href="#" id="get_org" onClick="javascript: get_org(\''.$addr.'\'); return false;">Организации</a>';
-		//} else geocode_local($_GET['lat'],$_GET['lon'],$_GET['addressdetails']);
-		//echo geocode($_GET['geocode']);
 	}
 
 	if(isset($_GET['company'])) {
 		$addr=$_GET['addr'];
 
 		$url='http://maps.yandex.ru/?text='.urlencode($addr).'&source=form&output=json';
-		/*$url='http://maps.yandex.ru/?where=87.1360435%2C53.7564775&sll=87.1360435%2C53.7564775&sspn=0.004097000000001572%2C0.002427000000004398&source=location&output=json';
-		$url='http://maps.yandex.ru/?text='.urlencode($addr).'&sspn=0.004097000000001572%2C0.002427000000004398&source=location&output=json';*/
-
 		$result = file_get_contents($url);
 
 		$json=json_decode($result);
 
 		$item = $json->{'vpage'}->{'data'}->{'locations'}->{'GeoObjectCollection'}->{'features'}[0]->{'properties'}->{'Businesses'}->{'items'};
-		/*$item = $json->{'vpage'}->{'data'}->{'businesses'}->{'GeoObjectCollection'}->{'features'};
-
-		echo '<pre>';
-		print_r($json->{'vpage'}->{'data'}->{'businesses'}->{'GeoObjectCollection'}->{'features'});
-		echo '</pre>';*/
-		
-/*		echo '<div style="float: left;">'.geocode_local($_GET['lat'],$_GET['lon'],$_GET['addressdetails']).'</div>
-					<div style="float: right;">&nbsp;<input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></input></div>';
-*/
 
 		if($item) {
 			echo '<div style="float: left;">Организации:</div>
@@ -124,14 +84,7 @@
 					<div style="float: left; width: 100%;">';
 			foreach ($item AS $key => $value) {
 				echo $key+1 .'.&nbsp;';
-				//echo '<a href="#" oid="'.$value->{'id'}.'" onClick="$(\'#chicken_contentDiv\').html(\''.$value->{'id'}.'\');">'.$value->{'name'}.'</a><br>';
-				//echo '<a href="#" onClick="javascript: get_company_detail(\''.$value->{'id'}.'\');">'.$value->{'name'}.'</a><br>';
 				echo '<a href="http://maps.yandex.ru/?ol=biz&oid='.$value->{'id'}.'" target="_blank">'.$value->{'name'}.'</a><br>';
-				//echo '<a href="http://maps.yandex.ru/?ol=biz&oid='.$value->{'properties'}->{'CompanyMetaData'}->{'id'}.'" target="_blank">'.$value->{'properties'}->{'CompanyMetaData'}->{'name'}.'</a><br>';
-				//http://maps.yandex.ru/?ol=biz&oid=1266199711
-				//echo '<hr>';
-				//print_r($value);
-				//echo '<br>';
 			}
 			echo '</div>';
 		}
@@ -150,11 +103,7 @@
 		$item = $json->{'vpage'}->{'data'}->{'locations'}->{'GeoObjectCollection'}->{'features'}[0]->{'properties'}->{'Businesses'}->{'items'};
 		print_r($json);
 		echo '</pre>';
-	
-		/*		echo '<div style="float: left;">'.geocode_local($_GET['lat'],$_GET['lon'],$_GET['addressdetails']).'</div>
-		 <div style="float: right;">&nbsp;<input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></input></div>';
-		*/
-	
+
 		if($item) {
 			echo '<div style="float: left;">Организации:</div>
 					<div style="float: right;">&nbsp;<input id="close_button" type="button" title="Закрыть" value="X" onClick="javascript: popupClear(); $(\'.alertify-logs\').html(\'\');"></input></div>
@@ -162,9 +111,6 @@
 			foreach ($item AS $key => $value) {
 				echo $key+1 .'.&nbsp;';
 				echo '<a href="#" oid="'.$value->{'id'}.'" onClick="$(\'#chicken_contentDiv\').html(\''.$value->{'id'}.'\');">'.$value->{'name'}.'</a><br>';
-				//echo '<hr>';
-				//print_r($value);
-				//echo '<br>';
 			}
 			echo '</div>';
 		}
@@ -182,10 +128,9 @@
 			echo $url;
 			die;
 			$result = file_get_contents($url);
-			
-			//$array = json_decode($result);
+
 			echo '<pre>';
-			 print_r($result);
+			print_r($result);
 			echo '</pre>';
 			die;
 			if($array->{'total'} > 0) {
@@ -200,7 +145,6 @@
 			}
 			die;
 		}
-		//echo geocode_addr($_GET['geocode_addr']);
 		echo 'ssss';
 		die;
 	}
